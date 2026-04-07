adb041fe66947858517e72fe2b7f97d4a119fe55
Sabid <sajjadsabid0@gmail.com>
2026-04-07 20:49:07 +0600

    first commit

---
+++ src/router/Router.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router';
+import RootLayout from '../layout/RootLayout';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        Component: RootLayout,
+ 
+    }
+]);
+
+export default router;
